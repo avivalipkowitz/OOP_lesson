ac06f1d6ccff1f85e0908fd8664d61c66d6cc081
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 17:46:02 -0700

    Added new characters, and ability to choose player

--- game.py
+++ game.py
@@ -4,6 +4,15 @@ from pyglet.window import key
 from core import GameElement
 import sys
 
+username = raw_input("To choose a player type 'Aviva' or 'Katie': ")
+
+if username == "Aviva":
+    princess = "Katie"
+else:
+    princess = "Aviva"
+
+print princess
+
 #### DO NOT TOUCH ####
 GAME_BOARD = None
 DEBUG = False
@@ -11,16 +20,25 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 5
-GAME_HEIGHT = 5
+GAME_WIDTH = 15
+GAME_HEIGHT = 10
 
 #### Put class definitions here ####
 
 class Rock(GameElement):
-    IMAGE = "Rock"
+    IMAGE = princess
+    SOLID = True
+    RETURN = False
 
 class Character(GameElement):
-    IMAGE = "Princess"
+    IMAGE = None
+
+    print dir(GameElement)
+
+    def __init__(self, image):
+        GameElement.__init__(self)
+        self.inventory = []
+        self.IMAGE = image
 
     def next_pos(self, direction):
         if direction == "up":
@@ -33,6 +51,28 @@ class Character(GameElement):
             return (self.x+1, self.y)
         return None
 
+
+class Gem(GameElement):
+    IMAGE = "BlueGem"
+    SOLID = False
+    RETURN = False
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d item(s)!" %(len(player.inventory)))
+
+
+class Heart(GameElement):
+    IMAGE = "Heart"
+    SOLID = False
+    RETURN = True
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a heart! You have %d item(s)!" %(len(player.inventory)))
+
+        next_x = 2
+        next_y = 2
 
 ####   End class definitions    ####
 
@@ -53,11 +93,28 @@ def keyboard_handler():
         next_x = next_location[0]
         next_y = next_location[1]
 
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, next_y, PLAYER)
+        # Check to see if there is an element already at those coordinates
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el:
+            existing_el.interact(PLAYER)
+
+        if existing_el is not None and existing_el.RETURN:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(2, 2, PLAYER)
+        elif existing_el is None or not existing_el.SOLID:
+            # If there's nothing there _or_ if the existing element is not solid, walk through
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
 
 def initialize():
     """Put game initialization code here"""
+
+    print princess
+
     rock_positions = [
             (2,1),
             (1,2),
@@ -70,21 +127,36 @@ def initialize():
         rock = Rock()
         GAME_BOARD.register(rock) 
         GAME_BOARD.set_el(pos[0],pos[1],rock)
-        rocks.append(rock) 
+        rocks.append(rock)
+
+    rocks[-1].SOLID = False 
 
     for rock in rocks:
         print rock
 
-    # In the initialize() function
+    GAME_BOARD.draw_msg("%s is trapped in the Campanile. Defeat all the Berkeley characters to get the key to free %s!!!" % (princess, princess))
+
+    # Initialize player
     global PLAYER
-    PLAYER = Character()
+    PLAYER = Character(username)
     GAME_BOARD.register(PLAYER)
     GAME_BOARD.set_el(2, 2, PLAYER)
     print PLAYER
 
     keyboard_handler()
 
-    GAME_BOARD.draw_msg("This game is wicked awesome.")
+    # Initialize gem
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
+
+    # Initialize heart
+    heart = Heart()
+    GAME_BOARD.register(heart)
+    GAME_BOARD.set_el(0, 0, heart)
+
+
+
     # GAME_BOARD.erase_msg()
 
 
